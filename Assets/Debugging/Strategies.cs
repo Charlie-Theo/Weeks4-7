eb8a3127d4077e14cb4b7bf9e378738094f62302
theodouc <theodouc@sheridancollege.ca>
2026-02-06 10:41:21 -0500

    Finished Week 6 Homework (Debugging).

--- Assets/Debugging/Strategies.cs
+++ Assets/Debugging/Strategies.cs
@@ -6,9 +6,23 @@ public class Strategies : MonoBehaviour
 
     void Start()
     {
+        //loop ten times
         for (int i = 0; i < 10; i++)
         {
-            Instantiate(prefab, new Vector3 (0, i/10, 0), Quaternion.identity);
+            //instantiate a prefab
+            //at a x position thats increasing by 1
+            //and a y position thats increasing by 0.1
+            //and 0 in z, no rotation
+
+            float x = i; //this shouldn't be 0
+
+            Debug.Log(i + "/" + 10 + " = " + i / 10);
+            float y = i / 10.0f; //this shouldn't be 0
+            Debug.Log("Y = " + y);
+
+            float z = 0; //this one is fine
+
+            Instantiate(prefab, new Vector3 (x, y, z), Quaternion.identity);
         }
     }
 
