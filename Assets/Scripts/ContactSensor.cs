09b4b1da7b693f318552f6577db9f8839750a4b4
theodouc <theodouc@sheridancollege.ca>
2026-02-17 10:49:30 -0500

    Contact sensor added.

---
+++ Assets/Scripts/ContactSensor.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ContactSensor : MonoBehaviour
+{
+    public SpriteRenderer hazard;
+    public bool isInHazard;
+
+    public UnityEvent onEnterHazard;
+    public UnityEvent onExitHazard;
+
+    public UnityEvent<float> onRandomNumber;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //is the player inside the hazard sprite?
+        if (hazard.bounds.Contains(transform.position) == true)
+        {
+            if (isInHazard == true)
+            {
+                //we're still in the hazard
+            }
+            else
+            {
+                //just entered the hazard, do something!
+                //Y: player has tripped the sensor
+                isInHazard = true;
+                Debug.Log("Entered the Hazard!");
+
+                onEnterHazard.Invoke();
+            }
+        }
+        else
+        {
+            if (isInHazard == true)
+            {
+                //we just left the hazard
+                //N: player has not tripped the sensor 
+                isInHazard = false;
+                Debug.Log("Exited the Hazard!");
+
+                onExitHazard.Invoke();
+                onRandomNumber.Invoke(Random.Range(0, 10));
+            }
+            else
+            {
+                //we're still not in the hazard
+            }
+        }
+    }
+
+    public void showNumber(float number)
+    {
+        Debug.Log(number);
+    }
+}
